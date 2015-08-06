cbc5f5ce25d3f1f5c4487a6c42b23315e16d77eb
AndriyBilyar <biliarandriy@i.ua>
2015-08-06 21:18:52 +0300

    homework 5

---
+++ Andrew/Andrew_homework5/gallery.js
@@ -0,0 +1,105 @@
+var NUMBERS_TO_DISPLAY = 0; // if 0 - all, if 1 - odd, if 2 - even
+var resultContainer = $('#result');
+var resultHTML = "";
+var itemTemplate = '<div class="col-sm-3 col-xs-6">\
+				<img src="$url" alt="$name" class="img-thumbnail">\
+				<div class="info-wrapper">\
+					<div class="text-muted">$number: $name</div>\
+					<div class="text-muted">$description</div>\
+					<div class="text-muted">$date</div>\
+				</div>\
+			</div>';
+
+// Это реальный объект на основе которого вам надо дудет строить галлерею
+var data = [{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40069.jpg",
+	name: "МАШИНКА",
+	id : 1,
+	description : " be conveyed to users of assistive technologies – such as",
+	date : 1422153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40068.jpg",
+	name: "ДРУГАЯ МАШИНКА",
+	id : 2,
+	description : "sing color to add meaning to a button",
+	date : 1421153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40067.jpg",
+	name: "НАЗВАНИЕ",
+	id : 3,
+	description : " be conveyed to users of assistive technologies",
+	date : 1426153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40066.jpg",
+	name: "ОПИСАНИЕ",
+	id : 4,
+	description : "ssistive technologies – such as screen readers. Ensure",
+	date : 1428153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40065.jpg",
+	name: "ИМЯ МАШИНЫ",
+	id : 5,
+	description : "color to add meaning to a button only provides",
+	date : 1402153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40064.jpg",
+	name: "МЕРСЕДЕС",
+	id : 6,
+	description : "om the content itself (the visible text of the button)",
+	date : 1442153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40063.jpg",
+	name: "БМВ",
+	id : 7,
+	description : "r is either obvious from the content itself",
+	date : 1482153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40062.jpg",
+	name: "ЛАДА КАЛИНА",
+	id : 8,
+	description : "included through alternative means, such as additional text hidden with the",
+	date : 1442153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40061.jpg",
+	name: "ДЖИП",
+	id : 9,
+	description : " meaning to a button only provides a visual",
+	date : 1322153200637
+},{
+	url: "http://desktopwallpapers.org.ua/mini/201507/40060.jpg",
+	name: "ДАЧА ЛОГАН",
+	id : 10,
+	description : "uded through alternative means, such as additional",
+	date : 1322159200637
+}];
+
+
+function extractDataToDisplay() {
+	return data.filter(function(item) {
+		if(NUMBERS_TO_DISPLAY == 1) {
+			return item.id % 2 != 0;
+		} else if(NUMBERS_TO_DISPLAY == 2) {
+			return item.id % 2 == 0;
+		} else return data;
+	});
+}
+
+
+extractDataToDisplay().forEach(function(item, i, arg) {
+
+	var capitalizedFirstLetter = item.name.charAt(0) + 
+								 item.name.slice(1).toLowerCase();
+	var shortDescription = item.description.slice(0, 15);
+	var date = new Date(item.date);
+	var formattedDate = date.getFullYear() + "/" + (+date.getMonth() + 1) 
+						+ "/" + date.getDate();
+	resultHTML += itemTemplate
+		.replace("$number", item.id)
+		.replace(/\$name/gi, capitalizedFirstLetter)
+		.replace("$url", item.url)
+		.replace("$description", shortDescription)
+		.replace("$date", formattedDate);
+});
+
+
+resultContainer.html(resultHTML);
