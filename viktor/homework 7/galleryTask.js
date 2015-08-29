edacd320c437333162b58dbfa064c581e2968b37
vitya11 <Ofir.cv@gmail.com>
2015-08-29 19:00:17 +0300

    Homework 7 remastred

--- viktor/homework 7/galleryTask.js
+++ viktor/homework 7/galleryTask.js
@@ -6,18 +6,20 @@ var maxCount = (data.length-1),
 
 
 var createCustomElement = function (config) {		
-	var el 			= document.createElement(config.type);
-	el.className 	= config.class;
-	if (config.src) {el.src = config.src};
+	var el 			        = document.createElement(config.type);
+	el.className 	        = config.class;
+	if (config.src) {el.src = config.src}
+    if (config.innerHTML) {el.innerHTML = config.innerHTML}
+    if (config.href) {el.hrefL = config.href}
 	config.parentElement.appendChild(el);
 	return el;
-}	
+};
 
-var updateCounts = function () {
-	currentCount == maxCount ? 0 : currentCount++;
-	count++;
-	$("#count").text(count);	
-}
+var updateCounts = function(event) {
+        event.id == 'addObj' ? (currentCount == maxCount ? currentCount = 0 : currentCount++, count++) :
+                               (currentCount--, count--);
+    document.querySelector('#count').innerHTML = count;
+};
 
 var newDate = function(date){
 		var tmpDate = new Date(date);
@@ -29,77 +31,83 @@ var newDate = function(date){
 };
 	
 var processNewElement = function(){
-	var mainDiv = document.getElementById('test');
+	var mainDiv = document.querySelector("#test");
+		
+    var secondDiv = createCustomElement ({
+        type 			: 'div',
+        class			: 'col-sm-3 col-xs-6',
+        parentElement 	: mainDiv
+    });
 
-	// var secondDiv = document.createElement('div');
-		// secondDiv.className = "col-sm-3 col-xs-6";
-		// mainDiv.appendChild(secondDiv);
-		
-		var secondDiv = createCustomElement ({
-			type 			: 'div',
-			class			: 'col-sm-3 col-xs-6',
-			parentElement 	: mainDiv
-		})
-		
-	// var imgURL = document.createElement('img');
-		// imgURL.src = data[currentCount].url;
-		// imgURL.className = 'img-thumbnail';
-		// secondDiv.appendChild(imgURL);
-		
-		
-		var imgURL = createCustomElement ({
-			type 			: 'img',
-			class			: 'img-thumbnail',
-			parentElement 	: secondDiv,
-			src				: data[currentCount].url
-		})
-			
-	var thirdDiv = document.createElement('div');
-		thirdDiv.className = "info-wrapper";
-		secondDiv.appendChild(thirdDiv);
-	 
-	var lastDiv1 = document.createElement('div');
-		lastDiv1.className = "text-muted";
-		lastDiv1.innerHTML = data[currentCount].id + ' ' + data[currentCount].name;
-		thirdDiv.appendChild(lastDiv1);
-		
-	var lastDiv2 = document.createElement('div');
-		lastDiv2.className = "text-muted";
-		lastDiv2.innerHTML = data[currentCount].description;
-		thirdDiv.appendChild(lastDiv2);
-		
-	var lastDiv3 = document.createElement('div');
-		lastDiv3.className = "text-muted";
-		lastDiv3.innerHTML = newDate(data[currentCount].date);
-		thirdDiv.appendChild(lastDiv3);
-		
-	var delDiv = document.createElement('div'),
-		delLink = document.createElement('a');
-		delDiv.className = "text-muted";
-		delLink.href = "#";
-		delLink.innerHTML = "Удалить";
-		thirdDiv.appendChild(delDiv);
-		delDiv.appendChild(delLink);
-		
-	updateCounts();	
+    var imgURL = createCustomElement ({
+        type 			: 'img',
+        class			: 'img-thumbnail',
+        parentElement 	: secondDiv,
+        src				: data[currentCount].url
+    });
 
-	
-}	
-	
-	
-$("#addObj").click(processNewElement);
+    var thirdDiv = createCustomElement ({
+        type 			: 'div',
+        class			: 'info-wrapper',
+        parentElement 	: secondDiv
+    });
 
+    var lastDiv1 = createCustomElement ({
+        type 			: 'div',
+        class			: 'text-muted',
+        innerHTML       : data[currentCount].id + ' ' + data[currentCount].name,
+        parentElement 	: thirdDiv
+    });
 
-$("#test").click(function(){
-		document.getElementById('test').removeChild(event.target.parentNode.parentNode.parentNode);
-		count--;
-		$("#count").text(count);
-	});
+    var lastDiv2 = createCustomElement ({
+        type 			: 'div',
+        class			: 'text-muted',
+        innerHTML       : data[currentCount].description,
+        parentElement 	: thirdDiv
+    });
+
+    var lastDiv3 = createCustomElement ({
+        type 			: 'div',
+        class			: 'text-muted',
+        innerHTML       : newDate(data[currentCount].date),
+        parentElement 	: thirdDiv
+    });
+
+    var delDiv = createCustomElement ({
+        type 			: 'div',
+        class			: 'text-muted',
+        parentElement 	: thirdDiv
+    });
+
+    var delLink = createCustomElement ({
+        type 			: 'a',
+        href			: '#',
+        class			: 'remove',
+        innerHTML       : 'Удалить',
+        parentElement 	: delDiv
+    });
+
+	updateCounts(this);
+};
+
+var delteElement = function(event){
+    if(event.target.className == 'remove'){
+         document.querySelector('#test').removeChild(event.target.closest('.col-sm-3'));
+         updateCounts(this)}
+};
+
+document.querySelector('#addObj').onclick = processNewElement;
+
+document.querySelector('#test').onclick = delteElement;
+
+	//document.querySelector('#test').removeChild(event.target.closest('.col-sm-3'));
+    //updateCounts();
+//};
 	
-	// сделать все через вызов функции createCustomElement
-	// вынести удаление в функцию и передать туда первый аргумент event
-	// поменять jquery подход на нативный
-	// getElementById -> querySelector
+	// сделать все через вызов функции createCustomElement +
+	// вынести удаление в функцию и передать туда первый аргумент event +
+	// поменять jquery подход на нативный +
+	// getElementById -> querySelector +
 
 	
 
